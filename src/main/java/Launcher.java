28f8b0ec062c9e219e0bfd662068c5edab7842b1
Fightyshy <tharonn@gmail.com>
2019-06-23 10:22:12 +0700

    Update Google Drive API auth to v3 Set API permissions scope to Drive Add platform neutral file seperator at 50

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -4,76 +4,78 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Launcher {
 
-    private static final String APPLICATION_NAME = "OpenCloseSync/1.1";
-    private static HttpTransport httpTransport;
-    private static final String DATA_STORE_DIR = "./userData"; //Storage for user creds, users home directory in subdirect
-    private static FileDataStoreFactory dataStoreFactory; //Best practice, shared across app
-    private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE_METADATA, DriveScopes.DRIVE);
-    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance(); //wtf is this???
+    private static final String APPLICATION_NAME = "OpenCloseSync";
+    private static JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static String TOKENS_DIRECTORY_PATH = "tokens";
+    private static Drive apiService;
     static Scanner userInput = new Scanner(System.in);
 
-    private static Credential authorize() throws Exception{ //authorizes app to access user data
-        InputStream sekritLoc = Launcher.class.getResourceAsStream("/client_secret.json");
-        GoogleClientSecrets loadSekrits = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(sekritLoc));
+    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE); //unsafe, need alt
+    private static final String CREDENTIALS_FILE_PATH =
+            ".."+File.separator+"resources"+File.separator+"credentials.json";
 
-        // Build flow and trigger user authorization request.
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = Launcher.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, JSON_FACTORY, loadSekrits, SCOPES)
-                .setDataStoreFactory(dataStoreFactory)
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    public static void main(String args[]) {
+    public static void main(String... args) throws IOException {
         System.out.println("1 for download file, 2 for upload file, 3 for exit.");
         System.out.print("Select an option: ");
         int option = userInput.nextInt();
 
         try{
-            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            dataStoreFactory = new FileDataStoreFactory(new File(DATA_STORE_DIR));
-
-            Credential credential = authorize();
-
-            Drive service = new Drive.Builder(httpTransport, JSON_FACTORY, credential).setApplicationName(APPLICATION_NAME).build();
-            DriveManager driver = new DriveManager();
-            // End auth, start activity
-            switch (option){
-                case 1: {
-                    String loc = driver.downloadFile(service);
-                    driver.runProcessor(loc);
-                    break;
-                }
-                case 2: {
-                    driver.killProcessor();
-                    driver.uploadFile(service);
-                    break;
-                }
-                case 3: System.exit(0);
-            }
+            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            apiService = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                    .setApplicationName(APPLICATION_NAME)
+                    .build();
         } catch(IOException e){
             System.out.println(e.getMessage());
             System.out.println("An error has occurred.");
         } catch (Throwable t) {
             t.printStackTrace();
+        }
+
+        // End auth, start activity
+        DriveManager driverSide = new DriveManager();
+        DesktopManager desktopSide = new DesktopManager();
+        switch (option){
+            case 1: {
+                String loc = driverSide.downloadFile(apiService);
+                desktopSide.runProgram(loc);
+                break;
+            }
+            case 2: {
+                desktopSide.killProgram();
+                driverSide.uploadFile(apiService);
+                break;
+            }
+            case 3: System.exit(0);
         }
     }
 }
